c7c83973a1275ef3549f2faf55847e6705029442
exponenta.euler <exponenta.euler@gmail.com>
2021-05-12 12:08:43 +0500

    done

--- WhiteBeltWeek2_CapitalList/WhiteBeltWeek2_CapitalList.cpp
+++ WhiteBeltWeek2_CapitalList/WhiteBeltWeek2_CapitalList.cpp
@@ -4,20 +4,78 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 int main()
 {
 	int q;
-	cout << "enter quieries count q: " << endl;
+	map<string, string> capitalList;
+	string cmd = "";
+	string country = "";
+	string capital = "";
+	cout << "Enter query count q: " << endl;
 	cin >> q;
 	while (q) {
+		cin >> cmd;
+		if (cmd == "DUMP") {
+			if (capitalList.size()) {
+				for (const auto& elem : capitalList) {
+					cout << elem.first << " " << elem.second << endl;
+				}
+			}
+			else {
+				cout << "There are no countries in the world. " << endl;
+			}
+		}
+		else {
+			cin >> country;
 
-
-
-
-		--q;
+			map<string, string>::iterator countryIterator = capitalList.find(country);
+			map<string, string>::iterator capitalListIterator = capitalList.end();
+			if (cmd == "ABOUT")
+			{
+				if (countryIterator != capitalListIterator)
+				{
+					cout << "Country " << country << " has capital " << countryIterator->second;
+				}
+				else cout << "Country " << country << " doesn't exist" << endl;
+			}
+			else
+			{
+				cin >> capital;
+				if (cmd == "RENAME")
+				{
+					auto iter_new_country = capitalList.find(capital);
+					if (capital == country ||
+						countryIterator == capitalListIterator ||
+						iter_new_country != capitalListIterator)
+						cout << "Incorrect rename, skip"<<endl;
+					else
+					{
+						cout << "Country " << country << " with capital " << capitalList[country] << " has been renamed to " << capital << endl;
+						capitalList[capital] = capitalList[country];
+						capitalList.erase(country);
+					}
+				}
+				else if (cmd == "CHANGE_CAPITAL")
+				{
+					if (capitalList[country] == capital)
+						cout << "Country " << country << " hasn't changed its capital." << endl;
+					else
+					{
+						if (countryIterator == capitalListIterator)
+							cout << "Introduce new country " << country << " with capital " << capital << endl;
+						else if (capitalList[country] != capital)
+							cout << "Country " << country << " has changed its capital from " <<
+							capitalList[country] << " to " << capital << endl;
+						capitalList[country] = capital;
+					}
+				}
+			}
+			--q;
+		}
 	}
 }
 
