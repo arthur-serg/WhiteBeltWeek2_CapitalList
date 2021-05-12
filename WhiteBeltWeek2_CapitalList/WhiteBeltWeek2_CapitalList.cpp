32131a4466d06dfeda8df1a9eef39b02b90d95ad
exponenta.euler <exponenta.euler@gmail.com>
2021-05-12 11:27:12 +0500

    initial

---
+++ WhiteBeltWeek2_CapitalList/WhiteBeltWeek2_CapitalList.cpp
@@ -0,0 +1,23 @@
+﻿// WhiteBeltWeek2_CapitalList.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int q;
+	cout << "enter quieries count q: " << endl;
+	cin >> q;
+	while (q) {
+
+
+
+
+		--q;
+	}
+}
+
